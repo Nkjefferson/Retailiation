c6a7825b96034d2c92851aaef0b69d1949ff13fe
jveg <jordanveg@gmail.com>
2026-01-11 00:43:08 -0500

    Incorporated shop/loadout scene between levels. Loadout and Coins now persist in a gamestate singleton

--- characters/player/cards/entangle_card/entangle_card.gd
+++ characters/player/cards/entangle_card/entangle_card.gd
@@ -3,17 +3,17 @@ extends Card
 @export var entangle_ability : PackedScene
 
 func _on_body_entered(body):
-	if body.is_in_group("enemies"):
-		body.take_damage(self)
-		# Level the audio based on distance from player on impact
-		MusicManager.play_sound_effect(enemy_hit_sound,-abs((global_position-parent_object.global_position).length())/25)
-	else:
-		MusicManager.play_sound_effect(wall_hit_sound,-abs((global_position-parent_object.global_position).length())/25)
-	if entangle_ability:
-		var ability = entangle_ability.instantiate()
-		ability.global_position = global_position
-		ability.rotation_degrees = rotation_degrees + 90
-		get_parent().call_deferred("add_child",ability)
-	else:
-		printerr("Failed to produce AOE no PackedScene selected")
-	queue_free()
+    if body.is_in_group("enemies"):
+        body.take_damage(self)
+        # Level the audio based on distance from player on impact
+        MusicManager.play_sound_effect(enemy_hit_sound,-abs((global_position-parent_object.global_position).length())/25)
+    else:
+        MusicManager.play_sound_effect(wall_hit_sound,-abs((global_position-parent_object.global_position).length())/25)
+    if entangle_ability:
+        var ability = entangle_ability.instantiate()
+        ability.global_position = global_position
+        ability.rotation_degrees = rotation_degrees + 90
+        get_parent().call_deferred("add_child",ability)
+    else:
+        printerr("Failed to produce AOE no PackedScene selected")
+    queue_free()
--- resources/game_state/game_state.gd
+++ resources/game_state/game_state.gd
@@ -1,5 +1,15 @@
 extends Node
 
+var player_currency : int = 0
+#TODO: Store as a CardInfo Resource instead of the scene itself
+var player_loadout : Array[PackedScene] = [
+	preload("res://characters/player/cards/basic_card/basic_card.tscn"),
+	preload("res://characters/player/cards/piercing_card/piercing_card.tscn"),
+	preload("res://characters/player/cards/triple_card/triple_card.tscn"),
+	#null,
+	preload("res://characters/player/cards/entangle_card/entangle_card.tscn"),
+	preload("res://characters/player/cards/orbit_card/orbit_card.tscn")
+]
 
 func save_game_state(setting):
 	var save_path = type_to_file_path(setting)
@@ -8,10 +18,10 @@ func save_game_state(setting):
 		return
 	print("Saving to " + save_path)
 	ResourceSaver.save(setting,save_path,ResourceSaver.FLAG_NONE)
-	
+
 func save_exists(path):
 	return ResourceLoader.exists(path)
-	
+
 func load_game_state(setting):
 	var save_path = type_to_file_path(setting)
 	print("Loading: " + save_path)
@@ -21,7 +31,7 @@ func load_game_state(setting):
 	return setting
 
 
-	
+
 func type_to_file_path(typename):
 	if not typename.has_method("print_name"):
 		printerr("This type does not have a print_name() function please implement one")
